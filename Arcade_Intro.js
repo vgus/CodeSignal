3d4837e00589323d4c78e10cb3a6f32a09a84ad8
vgus <uvkoking@gmail.com>
2021-11-08 10:40:57 -0600

    Primer acercamiento a la solucion de  stringsRearrangement

--- Arcade_Intro.js
+++ Arcade_Intro.js
@@ -594,6 +594,109 @@ function getColor(cell){
 
 //#region Through the Fog
 
+function circleOfNumbers(n, firstNumber) {
+    let oposite = (n/2)+firstNumber
+    if (oposite>=n)
+        oposite = oposite-n
+    return oposite
+}
+
+
+function depositProfit(deposit, rate, threshold) {
+    let year = 1
+    let balance = deposit+(deposit*rate/100)
+    if(balance>=threshold)
+    {
+        return year  
+    }
+    else
+    {
+        year += 1
+        year = depositProfit(balance, rate, threshold) + 1
+        return year
+    }
+}
+
+function absoluteValuesSumMinimization(a) {
+    
+    vecSum = []
+    for (let i = 0;i<a.length;i++)
+    {
+        let sum = 0
+        for (let j = 0;j<a.length;j++)
+        {
+            sum += Math.abs(a[i]-a[j])
+        }
+        vecSum.push(sum)
+    }
+    //console.table(vecSum)
+    minSum = Math.min(...vecSum)
+    return a[vecSum.findIndex(value => value == minSum)]
+
+}
+
+function stringsRearrangement(inputArray) {
+    let numElem = inputArray.length
+    var salida = false
+    if (numElem == 1)
+    {
+        console.log(inputArray)
+        salida = true
+        return salida
+    }
+    else
+    {
+        let element = inputArray[0]
+        
+        for (let i= 0; i<numElem;i++)
+        {
+            let sal = [...inputArray]
+            let elemChang = inputArray[i]
+            sal[0] = elemChang
+            sal[i] = element   
+            console.log(sal)
+            let evalArray = sal.slice(1)
+            let nextElem = evalArray[0]
+            
+            
+            if ((diffByAChar(element,elemChang))&&diffByAChar(elemChang,nextElem))
+            {
+                
+                salida = stringsRearrangement(evalArray)
+                if(salida)
+                    break
+            }
+            else
+                continue
+        }
+    }    
+   return salida
+    
+}
+
+function diffByAChar(str1, str2)
+{
+    var count = 0
+    var sal = true
+    for(let i=0;i<str1.length;i++)
+    {
+        if(str1[i]!=str2[i])
+            count += 1
+        if (count>1)
+        {
+            sal = false
+            break
+        }
+         
+    }
+    if (count==0)
+    {
+        sal = false
+        return sal
+    }
+    else
+        return sal
+}
 
 
 //#endregion Through the Fog
@@ -737,10 +840,61 @@ console.log(variableName(name))
 inputString = "crazy"
 console.log(alphabeticShift(inputString))7
 
-*/
 
-//#endregion Ejemplos
 
 cell1 = "A1" 
 cell2 = "C3"
 console.log(chessBoardCellColor(cell1, cell2)) // true.
+
+
+
+
+n = 10
+firstNumber = 2
+
+n = 10
+firstNumber = 7
+
+n= 6
+firstNumber= 3
+
+console.log(circleOfNumbers(n, firstNumber)) // 7
+
+deposit = 100
+rate = 20
+threshold = 170
+
+deposit= 100
+rate= 1
+threshold= 101
+
+console.log(depositProfit(deposit, rate, threshold))// = 3
+
+a =[2, 4, 7]
+//a = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150]
+//respuesta 65
+
+
+console.log(absoluteValuesSumMinimization(a)) //4.
+
+
+*/
+
+//#endregion Ejemplos
+
+//inputArray = [1,2,3]
+//inputArray = [6,5,4,3,1,1,2,1]
+//inputArray = [1,1,1,1,1]
+//inputArray = ["aba", "bbb", "bab"] //false
+//inputArray = ["ab", "bb", "aa"] //true
+//inputArray=["a",  "b",  "c"]
+//inputArray=["q", "q"] // false
+//inputArray=["abc",  "bef",  "bcc",  "bec",  "bbc",  "bdc"] //true
+//inputArray=["abc", "abx", "axx", "abc"] //false
+inputArray=["abc",  "abx",  "axx",  "abx",  "abc"] // true
+//inputArray=["abc", "abx", "axx", "abc"] //false
+
+//inputArray=["abc",  "bef",  "bcc",  "bec",  "bbc",  "bdc"] //true
+
+console.log(stringsRearrangement(inputArray))// = false.
+//console.log(diffByAChar("bbc","abc"))
